54349dfcf00ee11cb651c9d9c6a421b8e42267e4
Zachary Watkins <watkinza@gmail.com>
2017-05-03 14:21:19 -0500

    Create home page template with custom fields

--- view/home.php
+++ view/home.php
@@ -10,11 +10,20 @@ remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
 // Add content
 add_action( 'genesis_after_header', 'research_home_slider' );
 add_action( 'genesis_entry_content', 'research_home_content' );
-add_action( 'genesis_entry_content', 'research_home_sidebar' );
+add_action( 'genesis_before_sidebar_widget_area', 'research_home_sidebar' );
+add_action( 'genesis_entry_content', 'research_home_template_programs' );
+
+// Remove content
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
 
 // Modify content
 add_filter( 'soliloquy_output_caption', 'research_soliloquy_title_before_caption', 10, 5 );
-
+add_filter( 'soliloquy_output_before_caption', 'research_soliloquy_caption_link', 9999, 5 );
+add_filter( 'soliloquy_output_after_caption', 'research_soliloquy_caption_close_link' );
+add_filter( 'soliloquy_output_container_style', 'research_soliloquy_container_style', 9999, 5 );
+add_filter( 'soliloquy_pre_data', 'research_soliloquy_set_dimensions', 9999, 5 );
+add_filter( 'feedzy_meta_output', 'research_feedzy_changemeta' );
+add_filter( 'feedzy_global_output', 'research_feedzy_customoutput' );
 
 function research_home_slider() {
 
@@ -35,21 +44,6 @@ function research_home_slider() {
 
 }
 
-function research_soliloquy_title_before_caption( $caption, $id, $slide, $data, $i ) {
-
-    // Check if current slide has a title specified
-    $object = get_field( 'select_carousel_slider' );
-    $title = $object->post_name;
-    $this_title = $data['config']['slug'];
-
-    if ( isset( $slide['title'] ) && !empty( $slide['title'] ) ) {
-        $caption = '<h3 class="title">' . $slide['title'] . '</h3>';
-        $caption .= '<div class="caption">' . $slide['caption'] . '</div>';
-    }
-
-    return $caption;
-}
-
 function research_home_content() {
 
     ?>
@@ -66,12 +60,132 @@ function research_home_content() {
 
 function research_home_sidebar() {
 
-    // Show date and title of 5 latest events with links
+    if ( is_active_sidebar( 'home-sidebar' ) ) {
 
-    // Show date and title of 5 latest posts from AgriLife Today with links
-    // http://today.agrilife.org/feed/
+        dynamic_sidebar( 'home-sidebar' );
+
+    }
 
 }
 
+function research_home_template_programs() {
+
+    if ( get_field( 'programs' ) && have_rows( 'programs' ) ) {
+
+        $count = (int)get_field( 'columns' );
+        $zfcols = (12 - 12 % $count) / $count;
+        $align = get_field( 'text_alignment' );
+        $programs = get_field( 'programs' );
+        $i = 0;
+
+        $content = '<div class="programs"><div class="row">';
+
+        foreach( $programs as $program ){
+
+            $heading = $program['heading'];
+            $img = $program['image'];
+            $desc = $program['description'];
+            $link = $program['link'];
+            $linkopen = $linkclose = '';
+
+            if( $link != '' ){
+                $linkopen = '<a href="' . $link . '">';
+                $linkclose = '</a>';
+            }
+
+            if( $heading != '' ){
+                $heading = '<h3>' . $heading . '</h3>';
+            }
+
+            if( $img != '' ){
+                $img = sprintf( '<img src="%s" alt="%s" title="%s" />', $img['sizes']['medium_large'], $img['alt'], $img['title'] );
+            }
+
+            $content .= sprintf( '<div class="small-12 medium-%s large-%s columns">%s%s%s%s%s</div>', $zfcols, $zfcols, $linkopen, $heading, $img, $linkclose, $desc );
+
+            // Ensure each row of columns is contained
+            $i++;
+            if( $i % $count == 0 && $i < count($programs) ){
+                $content .= '</div><div class="row">';
+            }
+
+        }
+
+        $content .= '</div></div>';
+
+        echo $content;
+
+    }
+
+}
+
+function research_soliloquy_title_before_caption( $caption, $id, $slide, $data, $i ) {
+
+    // Check if current slide has a title specified
+    $object = get_field( 'select_carousel_slider' );
+    $title = $object->post_name;
+    $this_title = $data['config']['slug'];
+
+    if ( isset( $slide['title'] ) && !empty( $slide['title'] ) ) {
+        $caption = '<h3 class="title">' . $slide['title'] . '</h3>';
+        $caption .= '<div class="caption">' . $slide['caption'] . '</div>';
+    }
+
+    return $caption;
+}
+
+function research_soliloquy_caption_link( $caption, $id, $slide, $data, $i ) {
+
+    if ( isset( $slide['link'] ) && !empty( $slide['link'] ) ) {
+        $caption = $caption . '<a href="' . $slide['link'] . '" class="soliloquy-caption">';
+    }
+
+    return $caption;
+}
+
+function research_soliloquy_caption_close_link( $caption, $id, $slide, $data, $i ) {
+    return '</a>' . $caption;
+}
+
+function research_soliloquy_container_style( $styles ){
+
+    return preg_replace('/margin[^;]+;/', 'margin: 0 auto;', $styles);
+
+}
+
+function research_soliloquy_set_dimensions( $data, $slider_id ){
+
+    if ( wp_is_mobile() ) {
+        return false;
+    } else {
+        $data['config']['slider_size'] = 'full_width';
+        $data['config']['slider_width'] = 0;
+        $data['config']['slider_height'] = 0;
+    }
+
+    return $data;
+
+}
+
+function research_feedzy_changemeta( $meta, $feedURL ){
+
+    preg_match('/on (\w+\s\d+,\s\d+) at/', $meta, $matches);
+
+    return $matches[1];
+
+}
+
+function research_feedzy_customoutput( $content, $sc, $feed_title, $feed_items ){
+
+    // Remove inline styles
+    $content = preg_replace('/\s+style="[^"]*"/', '', $content);
+    // Remove small tags
+    $content = preg_replace('/<\/?small>/', '', $content);
+    // Switch title and meta
+    $content = preg_replace('/(<span class="title">[^<]+<a[^>]+>[^<]+<\/a>[^<]+<\/span>)([^<]+)(<div class="rss_content">[^<]+<\/div>)/', '$3$2$1', $content);
+
+    return $content;
+
+}
 
 genesis();
