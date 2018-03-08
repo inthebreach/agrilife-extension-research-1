48967db364785e0d0698a0dd8720634e4a82f440
Zachary Watkins <watkinza@gmail.com>
2018-03-08 10:11:28 -0600

    Remove custom research project banner image size and implement image cropping

--- fields/banner-details.php
+++ fields/banner-details.php
@@ -2,16 +2,11 @@
 
 if( function_exists('acf_add_local_field_group') ):
 
-acf_add_local_field_group(array (
+acf_add_local_field_group(array(
 	'key' => 'group_58f7ba1d9ef3c',
 	'title' => 'Banner',
-	'fields' => array (
-		array (
-			'default_value' => '',
-			'maxlength' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
+	'fields' => array(
+		array(
 			'key' => 'field_5911e10f596da',
 			'label' => 'Title',
 			'name' => 'bannertitle',
@@ -19,18 +14,18 @@ acf_add_local_field_group(array (
 			'instructions' => '',
 			'required' => 0,
 			'conditional_logic' => 0,
-			'wrapper' => array (
+			'wrapper' => array(
 				'width' => '',
 				'class' => '',
 				'id' => '',
 			),
-		),
-		array (
 			'default_value' => '',
-			'new_lines' => 'wpautop',
 			'maxlength' => '',
 			'placeholder' => '',
-			'rows' => 4,
+			'prepend' => '',
+			'append' => '',
+		),
+		array(
 			'key' => 'field_5911e11c596db',
 			'label' => 'Description',
 			'name' => 'bannerdescription',
@@ -38,52 +33,57 @@ acf_add_local_field_group(array (
 			'instructions' => '',
 			'required' => 0,
 			'conditional_logic' => 0,
-			'wrapper' => array (
+			'wrapper' => array(
 				'width' => '',
 				'class' => '',
 				'id' => '',
 			),
+			'default_value' => '',
+			'new_lines' => 'wpautop',
+			'maxlength' => '',
+			'placeholder' => '',
+			'rows' => 4,
 		),
-		array (
-			'return_format' => 'array',
-			'preview_size' => 'full',
-			'library' => 'all',
-			'min_width' => 1024,
-			'min_height' => '',
-			'min_size' => '',
-			'max_width' => '',
-			'max_height' => '',
-			'max_size' => '',
-			'mime_types' => '',
+		array(
 			'key' => 'field_58f7ba36e089a',
 			'label' => 'Image',
 			'name' => 'bannerimage',
-			'type' => 'image',
+			'type' => 'image_crop',
 			'instructions' => '',
 			'required' => 0,
 			'conditional_logic' => 0,
-			'wrapper' => array (
+			'wrapper' => array(
 				'width' => '',
 				'class' => '',
 				'id' => '',
 			),
+			'crop_type' => 'min',
+			'target_size' => 'custom',
+			'width' => 1024,
+			'height' => 210,
+			'preview_size' => 'medium',
+			'force_crop' => 'no',
+			'save_in_media_library' => 'yes',
+			'retina_mode' => 'no',
+			'save_format' => 'object',
+			'library' => 'all',
 		),
 	),
-	'location' => array (
-		array (
-			array (
+	'location' => array(
+		array(
+			array(
 				'param' => 'post_type',
 				'operator' => '==',
-				'value' => 'page',
+				'value' => 'post',
 			),
-			array (
+			array(
 				'param' => 'page_template',
 				'operator' => '!=',
-				'value' => AG_EXTRES_TEMPLATE_PATH . '/home.php',
+				'value' => 'default',
 			),
 		),
-		array (
-			array (
+		array(
+			array(
 				'param' => 'post_type',
 				'operator' => '==',
 				'value' => 'research-project',
